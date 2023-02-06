34cf7900e12b952287ecb1912946f65fd4a1e0e9
Kevin Saputra <kevputra@gmail.com>
2023-02-06 16:33:11 +0700

    About me updated

--- src/pages/AboutMe.js
+++ src/pages/AboutMe.js
@@ -1,5 +1,18 @@
 import React from "react"
+import Image from "./Assets/Kepin.png"
 
 export default function AboutMe() {
-  return <h1>AboutMe</h1>
+  return (
+    <div className="homeContainer">
+      <img className="kevinImg" src={Image} alt="image kepin" />
+      <div>
+        <p className="aboutMeDesc">
+          Hi, my name is Kevin Saputra. A friendly attitude and passionate in
+          the front-end developer industry whose handsomely work in a team or
+          individually and also currently looking for an internship. I am
+          willing and more than motivated to learn more from experience
+        </p>
+      </div>
+    </div>
+  )
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,14 +2,12 @@ import Image from "./Assets/Kepin.png"
 
 export default function Home() {
   return (
-    <>
-      <div className="homeContainer">
-        <img className="kevinImg" src={Image} alt="image kepin" />
-        <div className="text-desc">
-          <h1>Kevin Saputra</h1>
-          <h1>Front-End Internship</h1>
-        </div>
+    <div className="homeContainer">
+      <img className="kevinImg" src={Image} alt="image kepin" />
+      <div className="text-desc">
+        <h1>Kevin Saputra</h1>
+        <h1>Front-End Internship</h1>
       </div>
-    </>
+    </div>
   )
 }
